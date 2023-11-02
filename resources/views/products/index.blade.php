e62251d3508956aca9c114f149cea8b009efa2f5
waqasbutt032 <mrw.ahmad@outlook.com>
2023-11-02 13:00:35 +0500

    Product database, index and product views created

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Laravel CRUD</title>
+	
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+</head>
+<body>
+	<nav class="navbar navbar-expand-sm bg-light">
+
+	  <!-- Links -->
+	  <ul class="navbar-nav">
+	    <li class="nav-item">
+	      <a class="nav-link" href="/">Products</a>
+	    </li>
+	  </ul>
+
+	</nav>
+
+	<div class="container">
+		<div class="text-right">
+			<a href="product/create" class="btn btn-info mt-2" role="button">New Product</a>
+		</div>
+	</div>
+	<div class="container">
+		<h1>Products</h1>
+	</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
+Route::get('product/create', [ProductController::class, 'create'])->name('products.create');
